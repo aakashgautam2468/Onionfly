54639ecd21388a732a8e3f49fd113f9e42d1570d
aakashgautam2468 <>
2025-04-18 16:37:25 +0530

    Last Sync: 2025-04-18 11:07:25 (Mobile)

---
+++ controllers/Pages.cc
@@ -0,0 +1,6 @@
+#include "Pages.h"
+
+void Pages::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
